48454a6492322fa0ed8140dd650ba94cdcdf973e
clara-lupa <71439281+clara-lupa@users.noreply.github.com>
2020-11-24 14:24:16 +0100

    Update providers_controller.rb

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -2,7 +2,7 @@ class ProvidersController < ApplicationController
   def index
     @user = current_user
     if params[:query].present?
-      sql_query = "first_name ILIKE :query OR last_name ILIKE :query OR title ILIKE :query OR speciality ILIKE :query OR address ILIKE :query"
+      sql_query = "first_name ILIKE :query OR last_name ILIKE :query OR title ILIKE :query OR specialty ILIKE :query OR address ILIKE :query"
       @providers = Provider.where(sql_query, query: "%#{params[:query]}%")
     else
       @providers = Provider.all
