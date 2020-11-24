fb15e5d5ad9fc422e01cb919caae37325b8a7656
clara-lupa <71439281+clara-lupa@users.noreply.github.com>
2020-11-24 14:23:18 +0100

    Update provider.rb

--- app/models/provider.rb
+++ app/models/provider.rb
@@ -1,6 +1,6 @@
 class Provider < ApplicationRecord
   validates :first_name, presence: true
   validates :last_name, presence: true
-  validates :speciality, presence: true
+  validates :specialty, presence: true
   validates :address, presence: true
 end
