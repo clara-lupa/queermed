86ca079cdd90c806e5770993c83c941832e73c80
Magda Barthofer <magda.barthofer@gmail.com>
2020-11-25 20:31:43 +0100

    added style to sign in sign up

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 2020_11_25_154913) do
 
   # These are extensions that must be enabled in order to support this database
