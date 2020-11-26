027d9b8b1696aac3ca65d1422da23dd2d4f92577
Dan Emmanuel dan.emmanuel85@gmail.com <dan.emmanuel85@gmail.com>
2020-11-26 12:55:20 +0100

    functional favorites feature... just need styling

--- app/controllers/shortlists_controller.rb
+++ app/controllers/shortlists_controller.rb
@@ -4,10 +4,12 @@ class ShortlistsController < ApplicationController
     @shortlist.user = current_user
     @shortlist.provider = Provider.find(params[:provider_id])
     @shortlist.save
+    redirect_back(fallback_location: providers_path)
   end
 
   def destroy
     @shortlist = Shortlist.find(params[:id])
     @shortlist.destroy
+    redirect_to shortlists_index_path
   end
 end
