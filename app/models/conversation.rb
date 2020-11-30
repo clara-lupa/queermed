7c847dd2216657de02af36b7c885749936d861a1
clara wolff <cwo@systemli.org>
2020-11-30 13:06:40 +0100

    implemented private messaging backend

--- app/models/conversation.rb
+++ app/models/conversation.rb
@@ -1,9 +1,10 @@
 class Conversation < ApplicationRecord
   belongs_to :user1, class_name: "User"
   belongs_to :user2, class_name: "User"
+  belongs_to :provider
   has_many :messages, dependent: :destroy
 
-  validates :user1, :user2, presence: true
+  validates :user1, :user2, :provider, presence: true
   validate :has_two_different_users, if: :user1 && :user2
 
   def has_two_different_users
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,13 +2,30 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :user1_conversations, class_name: "Conversation", foreign_key: "user1_id", dependent: :destroy
-  has_many :user2_conversations, class_name: "Conversation", foreign_key: "user2_id"
+  has_many :user2_conversations, class_name: "Conversation", foreign_key: "user2_id", dependent: :destroy
   has_many :shortlists
   has_many :providers, through: :shortlists
+  # call .user2s on a User instance to get all users the instance has contacted
+  has_many :user2s, through: :user1_conversations, class_name: "User", source: "user2"
+  # call .user1s on a User instance to get all users the instance was contacted by
+  has_many :user1s, through: :user2_conversations, class_name: "User", source: "user1"
   has_one_attached :photo
   validates :avatar, presence: true
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  def conversation?(other_user, provider)
+    return false unless (user2s + user1s).include? other_user
+
+    if user2s.include? other_user
+      user1_conversations.find do |conversation|
+        conversation.user2 == other_user && conversation.provider == provider
+      end
+    else
+      user2_conversations.find do |conversation|
+        conversation.user1 == other_user && conversation.provider == provider
+      end
+    end
+  end
 end
