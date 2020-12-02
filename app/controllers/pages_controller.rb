a2f397edfd6e6950e8bd443bd4bb0f7bc55f02ca
clara-lupa <71439281+clara-lupa@users.noreply.github.com>
2020-12-02 16:49:45 +0100

    Update pages_controller.rb

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home, :index, :show]
+  skip_before_action :authenticate_user!, only: [:home]
 
   def home
   end
