9a0e4433d190bf343d368db9206e37a7dba27a7a
Igor Dzilit igor.dzilit@yahoo.com <igor.dzilit@yahoo.com>
2020-12-02 13:13:28 +0100

    devise settings, allowing non logged in userss to see index and show of the providers

--- app/controllers/providers_controller.rb
+++ app/controllers/providers_controller.rb
@@ -1,4 +1,6 @@
 class ProvidersController < ApplicationController
+  skip_before_action :authenticate_user!, only: [:index, :show]
+
   def index
     @user = current_user
     @location = params[:location]
