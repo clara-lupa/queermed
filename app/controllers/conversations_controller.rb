5b1c72cb0571f63a25a710d46d6bb24f6b46b001
clara wolff <cwo@systemli.org>
2020-12-22 19:31:09 +0100

    created and tested includes and where statement in conversations controller

--- app/controllers/conversations_controller.rb
+++ app/controllers/conversations_controller.rb
@@ -12,11 +12,16 @@ class ConversationsController < ApplicationController
 
   def index
     @user = current_user
-    @user1_conversations = @user.user1_conversations
-    @user2_conversations = @user.user2_conversations
-    @conversations = @user1_conversations + @user2_conversations
-    @conversations.reject! { |con| con.messages.empty? }
-    @conversations.sort_by! {|con| con.messages.last.created_at}
+    @conversations =
+      Conversation
+      .includes(:user1, :user2)
+      .where("user1_id=#{current_user.id} OR user2_id=#{current_user.id}")
+
+    # @user1_conversations = @user.user1_conversations
+    # @user2_conversations = @user.user2_conversations
+    # @conversations = @user1_conversations + @user2_conversations
+    # @conversations.reject! { |con| con.messages.empty? }
+    # @conversations.sort_by! { |con| con.messages.last.created_at }
   end
 
   def show
